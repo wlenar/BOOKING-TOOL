17e24f188e8e4e022b4956427bda145b7098196a
Wojtek <w_lenartowicz@yahoo.com>
2025-07-23 19:32:09 +0200

    feat: setup frontend with Vite, Tailwind v3, React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-yellow-300 p-10 space-y-4">
+      <h1 className="text-red-500 text-3xl font-bold">Czerwony napis</h1>
+      <h2 className="text-blue-500 text-2xl">Niebieski napis</h2>
+    </div>
+  );
+}
